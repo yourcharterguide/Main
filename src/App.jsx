90588a35321d64dbcd95dd55358d99a8519ca42e
yourcharterguide <info@yourcharter.guide>
2025-05-21 19:09:50 +0200

    Add basic questionnaire app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import Questionnaire from './components/Questionnaire.jsx';
+
 export default function App() {
-  return <div>Hello WhatCharter – dein Projekt wurde geladen.</div>;
+  return <Questionnaire />;
 }
+
